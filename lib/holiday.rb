2704cc32711d35ea73b3785f13e895e49dfe1d98
Kay B <kaylouisebennett@gmail.com>
2018-08-21 04:07:20 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ list = []
 holiday_hash.each do |season, holiday|
   puts"#{season.to_s.capitalize}"
   holiday_hash[season].each do |holiday, value|
-    
+    val = []
     puts "#{value.to_s.capitalize}"
   end
 end
